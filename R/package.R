f59681cb5d71f864b3fa093ebadc7179d8f7db8a
Herb Susmann <herbps10@gmail.com>
2026-09-10 22:29:53 +0100

    Updated to fix R CMD check issues

--- R/package.R
+++ R/package.R
@@ -3,6 +3,12 @@
 #' @description Fit Bayesian fertility schedule models, including the Peristera-Kostaki model
 #' @family help
 #' @importFrom instantiate stan_package_model
+#' @importFrom stats predict setNames
+#' @importFrom utils head
 NULL
 
 `%||%` <- function(a, b) if (is.null(a)) b else a
+
+# Column names referenced via NSE (ggplot2::aes(), tibble::tibble()'s `:=`)
+# that R CMD check's static analysis can't otherwise resolve.
+utils::globalVariables(c(".fitted", ".lower", ".upper", "age", "asfr", "value", ":="))
